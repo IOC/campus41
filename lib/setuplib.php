2f803eb5a861fdbfca859b0ea43171b98d7d1f1e
Israel Forés <israel.fores@ticxcat.cat>
2023-08-21 11:55:04 +0200

    Add IOC017 PATCH - Fixed login using https and BigIP

--- lib/setuplib.php
+++ lib/setuplib.php
@@ -976,7 +976,20 @@ function setup_get_remote_url() {
     }
     $rurl['port'] = (int)$_SERVER['SERVER_PORT'];
     $rurl['path'] = $_SERVER['SCRIPT_NAME']; // Script path without slash arguments
+
+    // @PATCH IOC@PATCH IOC017: Fixed login using https and BigIP
+    if (isset($_SERVER['HTTP_X_FORWARDED_PROTO'])) {
+        $rurl['scheme'] = $_SERVER['HTTP_X_FORWARDED_PROTO'];
+    } else if (!empty($_SERVER['HTTPS'])) {
+        $rurl['scheme'] = strtolower($_SERVER['HTTPS']) == 'off' ? 'http' : 'https';
+    } else {
+        $rurl['scheme'] = 'http';
+    }
+    // Original.
+    /*
     $rurl['scheme'] = (empty($_SERVER['HTTPS']) or $_SERVER['HTTPS'] === 'off' or $_SERVER['HTTPS'] === 'Off' or $_SERVER['HTTPS'] === 'OFF') ? 'http' : 'https';
+    */
+    // Fi.
 
     if (stripos($_SERVER['SERVER_SOFTWARE'], 'apache') !== false) {
         //Apache server
