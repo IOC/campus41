ad92b9d4d0929d19ebd769d36ecb21583579aa3a
Israel Forés <israel.fores@ticxcat.cat>
2023-08-22 08:36:13 +0200

    Add IOC037 PATCH - Correccions de la conversió de backups de Moodle 1.9

--- mod/forum/backup/moodle1/lib.php
+++ mod/forum/backup/moodle1/lib.php
@@ -61,7 +61,13 @@ class moodle1_mod_forum_handler extends moodle1_mod_handler {
                         'completionreplies' => 0,
                         'completionpost' => 0,
                         'maxattachments' => 1,
+                        // @PATCH IOC037: Correccions de la conversió de backups de Moodle 1.9.
+                        'introformat' => FORMAT_HTML,
+                        // Original.
+                        /*
                         'introformat' => 0,
+                        */
+                        //Fi
                     ),
                 )
             ),
