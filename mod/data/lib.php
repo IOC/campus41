b4e9f68be0458e31fe79eabcd43f07eead3a018c
Israel Forés <israel.fores@ticxcat.cat>
2023-08-22 08:45:42 +0200

    Add IOC038 PATCH - Límit del nombre d'entrades per pàgina

--- mod/data/lib.php
+++ mod/data/lib.php
@@ -1685,7 +1685,15 @@ function data_print_preference_form($data, $perpage, $search, $sort='', $order='
     }
     echo '<label for="pref_perpage">'.get_string('pagesize','data').'</label> ';
     $pagesizes = array(2=>2,3=>3,4=>4,5=>5,6=>6,7=>7,8=>8,9=>9,10=>10,15=>15,
+
+    // @PATCH IOC038: Límit del nombre d'entrades per pàgina.
+                       20=>20,30=>30,40=>40,50=>50,100=>100);
+    // Original.
+    /*
                        20=>20,30=>30,40=>40,50=>50,100=>100,200=>200,300=>300,400=>400,500=>500,1000=>1000);
+    */
+    // Fi.
+
     echo html_writer::select($pagesizes, 'perpage', $perpage, false, array('id' => 'pref_perpage', 'class' => 'custom-select'));
 
     if ($advanced) {
