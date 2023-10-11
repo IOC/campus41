b643f48a080bc64cb3464dc572f30d65adaf6b19
gduno <guillem.duno@ithinkupc.com>
2023-10-11 08:50:32 +0200

    question/type/regexp/

---
+++ question/type/regexp/version.php
@@ -0,0 +1,35 @@
+<?PHP
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version information for the Regexp question type.
+ *
+ * @package    qtype_regexp
+ * @copyright  2011-2022 Joseph Rézeau moodle@rezeau.org
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'qtype_regexp';
+$plugin->dependencies = array(
+    'qbehaviour_regexpadaptivewithhelp'   => 2022041800,
+    'qbehaviour_regexpadaptivewithhelpnopenalty'  => 2022041800
+);
+$plugin->version  = 2023051300;
+$plugin->requires = 2016052300; // Moodle version.
+$plugin->release = '4.2 for Moodle 4.2';
+$plugin->maturity  = MATURITY_STABLE;
